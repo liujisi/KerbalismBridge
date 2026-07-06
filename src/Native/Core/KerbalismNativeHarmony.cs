d033649788b07c456be439d20d9ca8701b067d68
Jisi Liu <jisi.liu@gmail.com>
2026-07-06 09:57:15 -0700

    test phase2c: split FixedUpdateFlight flags, disable converter-only

--- src/Native/Core/KerbalismNativeHarmony.cs
+++ src/Native/Core/KerbalismNativeHarmony.cs
@@ -17,22 +17,24 @@ namespace KerbalismNative
 
 			// Register individual patch classes so one failure doesn't abort others.
 			// 'false' arguments below disable selected patches for troubleshooting.
-			bool enableConverterPostProcess  = true;
-			bool enableHarvesterPostProcess  = true;
-			bool enableFixedUpdateFlight     = false;
-			bool enableFissionReactor        = true;
-			bool enableFissionDoCatchup      = true;
+			bool enableConverterPostProcess   = true;
+			bool enableHarvesterPostProcess   = true;
+			bool enableConverterFixedUpdate   = false;
+			bool enableHarvesterFixedUpdate   = true;
+			bool enableFissionReactor         = true;
+			bool enableFissionDoCatchup       = true;
 
 			TryPatch(harmony, typeof(Patch_FissionReactor_HandleResourceActivities), enableFissionReactor);
 			TryPatch(harmony, typeof(Patch_FissionReactor_DoCatchup), enableFissionDoCatchup);
 			TryPatch(harmony, typeof(Patch_SystemHeatConverter_PostProcess), enableConverterPostProcess);
 			TryPatch(harmony, typeof(Patch_SystemHeatHarvester_PostProcess), enableHarvesterPostProcess);
-			TryPatch(harmony, typeof(Patch_SystemHeatConverter_FixedUpdateFlight), enableFixedUpdateFlight);
-			TryPatch(harmony, typeof(Patch_SystemHeatHarvester_FixedUpdateFlight), enableFixedUpdateFlight);
+			TryPatch(harmony, typeof(Patch_SystemHeatConverter_FixedUpdateFlight), enableConverterFixedUpdate);
+			TryPatch(harmony, typeof(Patch_SystemHeatHarvester_FixedUpdateFlight), enableHarvesterFixedUpdate);
 
-			BridgeUtils.Log("Native core Layer B Harmony patches applied (converter=" + enableConverterPostProcess
-				+ " harvester=" + enableHarvesterPostProcess
-				+ " fixedUpdate=" + enableFixedUpdateFlight + ")");
+			BridgeUtils.Log("Native core Layer B Harmony patches applied (converterFixedUpdate=" + enableConverterFixedUpdate
+				+ " harvesterFixedUpdate=" + enableHarvesterFixedUpdate
+				+ " converterPostProcess=" + enableConverterPostProcess
+				+ " harvesterPostProcess=" + enableHarvesterPostProcess + ")");
 		}
 
 		private static void TryPatch(Harmony harmony, Type patchType, bool enabled)
